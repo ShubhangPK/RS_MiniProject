f0b90e6372a3f8d54e35ea8bf6657c49be2da1bc
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2019-04-26 21:35:30 +0530

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Breath' },
+  { id: 12, name: 'Wings of Fire' },
+  { id: 13, name: 'Into Thin Air' },
+  { id: 14, name: 'Mr. Feynman' },
+  { id: 15, name: 'The Lean Startup' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Living' }
+];
